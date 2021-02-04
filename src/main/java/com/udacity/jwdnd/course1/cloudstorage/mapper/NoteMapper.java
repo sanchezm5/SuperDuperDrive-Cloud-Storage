37a4d376abad1a61903418ad896a3fb9d4258697
Maria Sanchez <msanchez157@gmail.com>
2021-02-03 22:14:34 -0500

    Created NoteMapper interface

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -0,0 +1,40 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * MyBatis mapper classes (@Mapper-annotated interfaces to access tables) to handle interacting with the database.
+ */
+
+@Mapper
+@Component
+public interface NoteMapper {
+
+    @Results({
+            @Result(property = "noteId", column = "noteid"),
+            @Result(property = "noteTitle", column = "notetitle"),
+            @Result(property = "noteDescription", column = "notedescription"),
+            @Result(property = "userId", column = "userid")
+    })
+
+    @Select("SELECT * FROM NOTES WHERE userid = #{userId}")
+    List<Note> getNotes(Integer userId);
+
+    @Select("SELECT * FROM NOTES WHERE userid = #{userId} AND noteid = #{noteid}")
+    Note getNote(Integer userId, Integer noteId);
+
+    @Insert("INSERT INTO NOTES(notetitle, notedescription, userid) VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    int addNote(Note note);
+
+    @Update("UPDATE NOTES SET notetitle=#{noteTitle}, notedescription=#{noteDescription}, userid=#{userId} WHERE noteid=#{noteId}")
+    void editNote(Note note);
+
+    @Delete("DELETE FROM NOTES WHERE noteid=#{noteId}")
+    void deleteNote(Integer noteId);
+}
+
