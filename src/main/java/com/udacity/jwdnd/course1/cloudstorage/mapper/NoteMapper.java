b0c3d1dfb393f81301de851f6c699073dac2d88c
Maria Sanchez <msanchez157@gmail.com>
2021-02-04 20:34:48 -0500

    Edited the NoteMapper class

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -24,8 +24,8 @@ public interface NoteMapper {
     @Select("SELECT * FROM NOTES WHERE userid = #{userId}")
     List<Note> getNotes(Integer userId);
 
-    @Select("SELECT * FROM NOTES WHERE userid = #{userId} AND noteid = #{noteid}")
-    Note getNote(Integer userId, Integer noteId);
+    @Select("SELECT * FROM NOTES WHERE noteid = #{noteid}")
+    Note getNote(Integer noteId);
 
     @Insert("INSERT INTO NOTES(notetitle, notedescription, userid) VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
