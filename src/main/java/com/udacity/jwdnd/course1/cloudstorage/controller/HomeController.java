f8b7e7816e49a024f1d4fdb89e022727779f3088
Maria Sanchez <msanchez157@gmail.com>
2021-02-04 21:49:22 -0500

    Edited HomeController class to include list of added notes

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,16 +1,18 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import com.udacity.jwdnd.course1.cloudstorage.model.NoteForm;
 import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationUserService;
 import com.udacity.jwdnd.course1.cloudstorage.services.EncryptionService;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,12 +23,14 @@ public class HomeController {
     @Autowired
     private FileService fileService;
     @Autowired
+    private NoteService noteService;
+    @Autowired
     private AuthenticationUserService authenticationUserService;
     @Autowired
     private EncryptionService encryptionService;
 
     @GetMapping("/home")
-    public String getHomePage(Authentication authentication, Model model) {
+    public String getHomePage(@ModelAttribute("noteForm") NoteForm noteForm, Authentication authentication, Model model) {
         model.addAttribute("name", authenticationUserService.getLoggedInName());
 
         List<File> filesList;
@@ -36,8 +40,17 @@ public class HomeController {
             filesList = new ArrayList<>();
         }
 
+        List<Note> noteList;
+        try {
+            noteList = noteService.getNotes(authenticationUserService.getLoggedInUserId());
+        } catch (NullPointerException e) {
+            noteList = new ArrayList<>();
+        }
+
         model.addAttribute("files", filesList);
         model.addAttribute("fileSize", filesList.size());
+        model.addAttribute("notes", noteList);
+        model.addAttribute("noteSize", noteList.size());
         model.addAttribute("encryptService", encryptionService);
 
         return "home";
