39bf4226aca4a38c301c42f819d0b7f7f287e3b4
Maria Sanchez <msanchez157@gmail.com>
2021-02-25 00:02:55 -0500

    Edited the Login Controller class

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/LoginController.java
@@ -1,7 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
-import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationUserService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,47 +8,38 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
+    Logger log = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     AuthenticationManager authenticationManager;
 
-    Logger log = LoggerFactory.getLogger(LoginController.class);
-
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private AuthenticationUserService authenticatedUserService;
 
     @GetMapping(value={"/login", "/"})
     public String loginView() {
         return "login";
     }
 
-    @RequestMapping("/login-user")
-    public String loginUser(@RequestParam("username") String username, @RequestParam("password") String password) {
-
+    @PostMapping("/login")
+    public String loginUser(@RequestParam("username") String username, @RequestParam("password") String password, RedirectAttributes redirectAttributes) {
         Authentication result = this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-
         SecurityContextHolder.getContext().setAuthentication(result);
 
-        log.info("***** LOGIN CONTROLLER *****");
         User user = userService.getUser(username);
-        authenticatedUserService.setUser(username);
-        log.info(authenticatedUserService.getLoggedInUser().toString());
         if (user == null) {
-            return "error";
-        } else {
-            return "redirect:/home";
+            redirectAttributes.addFlashAttribute("loginError", true);
+            return "redirect:/login";
         }
+        log.info("***** USER INFO *****" + user.toString());
+        return "redirect:/home";
     }
 }
 
