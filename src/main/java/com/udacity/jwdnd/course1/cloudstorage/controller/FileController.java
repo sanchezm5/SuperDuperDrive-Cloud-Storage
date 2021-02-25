aba66d682ead44de475fb18568f883659e27d611
Maria Sanchez <msanchez157@gmail.com>
2021-02-25 00:00:25 -0500

    Edited the FileController class

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
@@ -1,7 +1,8 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.File;
-import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.services.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -12,50 +13,41 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/file")
 public class FileController {
 
-    private FileService fileService;
-
-    public FileController(FileService fileService) {
-        this.fileService = fileService;
-    }
+    @Autowired
+    private FileStorageService fileStorageService;
 
     @GetMapping("/{fileName}")
+    @ResponseBody
     public ResponseEntity<Resource> viewFile(@PathVariable String fileName) {
-        File fileToView = fileService.loadFile(fileName);
-        try {
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"" + fileToView.getFileName())
-                    .contentLength(fileToView.getFileData().length)
-                    .body(new ByteArrayResource(fileToView.getFileData()));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
+        File fileToView = fileStorageService.loadAsFile(fileName);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"" + fileToView.getFileName() + "\"")
+                .body(new ByteArrayResource(fileToView.getFileData()));
     }
 
     @GetMapping("/delete/{fileName}")
     public String deleteFile(@PathVariable String fileName, Authentication authentication, RedirectAttributes redirectAttributes) {
-        fileService.deleteFile(fileName);
+        fileStorageService.deleteFile(fileName);
         redirectAttributes.addFlashAttribute("message", "Successfully deleted " + fileName);
         return "redirect:/home";
     }
 
-    @PostMapping("/upload")
-    public String uploadFile(@RequestParam("fileUpload") MultipartFile multipartFile, Authentication authentication, RedirectAttributes redirectAttributes) {
+    @PostMapping("/")
+    public String uploadFile(@RequestParam("file") MultipartFile multipartFile, Authentication authentication, RedirectAttributes redirectAttributes) throws IOException {
         if (multipartFile.isEmpty()) {
-            redirectAttributes.addFlashAttribute("message","Please upload a file!");
+            redirectAttributes.addFlashAttribute("message","Please choose a file to upload!");
             return "redirect:/home";
-        } else if (!fileService.isFileNameAvailable(multipartFile.getOriginalFilename())){
-            redirectAttributes.addFlashAttribute("message","File with this name already exists!");
-            return "redirect:/home";
-        }
-        int row = fileService.uploadAFile(multipartFile);
-        if (row < 0) {
-            redirectAttributes.addFlashAttribute("message","Please try again!");
+        } else if (!fileStorageService.isFileNameAvailable(multipartFile.getOriginalFilename())){
+            redirectAttributes.addFlashAttribute("message","File name already exists! Try again.");
             return "redirect:/home";
         }
+        fileStorageService.uploadAFile(multipartFile);
         redirectAttributes.addFlashAttribute("message","Successfully uploaded " + multipartFile.getOriginalFilename());
         return "redirect:/home";
     }
